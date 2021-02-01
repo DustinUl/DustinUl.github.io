58e6079452fc7a67a8bca6378ee35c3bf64abd96
DustinUl <73825842+DustinUl@users.noreply.github.com>
2021-02-01 19:02:54 +0100

    Update Pong.js

--- Pong.js
+++ Pong.js
@@ -1,5 +1,7 @@
 Score = [0, 0];
 let r = 200;
+screen = 0;
+rotation = 1;
 
 var puck = {
   x: width / 2,
@@ -14,6 +16,174 @@ var paddles = {
   x2: width - 15,
   y2: height / 2 - 60,
 };
+
+function Startscreen() {
+  let y = 5;
+  let x = 5;
+  let r = 0;
+  background(0);
+  for (let i = 0; i < width; i = i + 25) {
+    fill(r + 255, r + 255, r + 255, 50);
+    noStroke();
+    ellipse(x + i, y, 10);
+    ellipse(x + i, y + 50, 10);
+    ellipse(x + i, y + 100, 10);
+    ellipse(x + i, y + 150, 10);
+    ellipse(x + i, y + 200, 10);
+    ellipse(x + i, y + 250, 10);
+    ellipse(x + i, y + 300, 10);
+    ellipse(x + i, y + 350, 10);
+    ellipse(x + i, y + 400, 10);
+    ellipse(x + i, y + 450, 10);
+    ellipse(x + i, y + 500, 10);
+    ellipse(x + i, y + 550, 10);
+    ellipse(x + i, y + 600, 10);
+    for (let i = 0; i < height; i = i + 25) {
+      fill(r + 255, r + 255, r + 255, 5);
+      noStroke();
+      ellipse(x, y + i, 10);
+      ellipse(x + 50, y + i, 10);
+      ellipse(x + 100, y + i, 10);
+      ellipse(x + 150, y + i, 10);
+      ellipse(x + 200, y + i, 10);
+      ellipse(x + 250, y + i, 10);
+      ellipse(x + 300, y + i, 10);
+      ellipse(x + 350, y + i, 10);
+      ellipse(x + 400, y + i, 10);
+      ellipse(x + 450, y + i, 10);
+      ellipse(x + 500, y + i, 10);
+      ellipse(x + 550, y + i, 10);
+      ellipse(x + 600, y + i, 10);
+      ellipse(x + 650, y + i, 10);
+    }
+  }
+  stroke(r + 255, r + 255, r + 255);
+  strokeWeight(4);
+  fill(r + 50, r + 205, r + 50);
+  textSize(90);
+  textStyle(BOLD);
+  text("RETRO PONG", width / 2 - 305, 110);
+  textSize(20);
+  textStyle(ITALIC);
+  rect(width / 2 - 108, 300, 200, 30);
+  fill(0);
+  text("Start", width / 2 - 30, 323);
+}
+
+function WinscreenP1() {
+  let y = 5;
+  let x = 5;
+  push();
+  translate(x, y);
+  rotate(rotation);
+  background(0);
+  for (let i = 0; i < width; i = i + 25) {
+    fill(255, 255, 255, 50);
+    noStroke();
+    ellipse(x + i, y, 10);
+    ellipse(x + i, y + 50, 10);
+    ellipse(x + i, y + 100, 10);
+    ellipse(x + i, y + 150, 10);
+    ellipse(x + i, y + 200, 10);
+    ellipse(x + i, y + 250, 10);
+    ellipse(x + i, y + 300, 10);
+    ellipse(x + i, y + 350, 10);
+    ellipse(x + i, y + 400, 10);
+    ellipse(x + i, y + 450, 10);
+    ellipse(x + i, y + 500, 10);
+    ellipse(x + i, y + 550, 10);
+    ellipse(x + i, y + 600, 10);
+    for (let i = 0; i < height; i = i + 25) {
+      fill(255, 255, 255, 5);
+      noStroke();
+      ellipse(x, y + i, 10);
+      ellipse(x + 50, y + i, 10);
+      ellipse(x + 100, y + i, 10);
+      ellipse(x + 150, y + i, 10);
+      ellipse(x + 200, y + i, 10);
+      ellipse(x + 250, y + i, 10);
+      ellipse(x + 300, y + i, 10);
+      ellipse(x + 350, y + i, 10);
+      ellipse(x + 400, y + i, 10);
+      ellipse(x + 450, y + i, 10);
+      ellipse(x + 500, y + i, 10);
+      ellipse(x + 550, y + i, 10);
+      ellipse(x + 600, y + i, 10);
+      ellipse(x + 650, y + i, 10);
+    }
+  }
+  stroke(255, 255, 255);
+  strokeWeight(4);
+  fill(50, 205, 50);
+  textSize(150);
+  textStyle(BOLD);
+  text("PLAYER1", width / 2 - 335, 300);
+  text("WINS", width / 2 - 200, 450);
+}
+
+function WinscreenP2() {
+  let y = 5;
+  let x = 5;
+  push();
+  translate(x, y);
+  rotate(rotation);
+  background(0);
+  for (let i = 0; i < width; i = i + 25) {
+    fill(255, 255, 255, 50);
+    noStroke();
+    ellipse(x + i, y, 10);
+    ellipse(x + i, y + 50, 10);
+    ellipse(x + i, y + 100, 10);
+    ellipse(x + i, y + 150, 10);
+    ellipse(x + i, y + 200, 10);
+    ellipse(x + i, y + 250, 10);
+    ellipse(x + i, y + 300, 10);
+    ellipse(x + i, y + 350, 10);
+    ellipse(x + i, y + 400, 10);
+    ellipse(x + i, y + 450, 10);
+    ellipse(x + i, y + 500, 10);
+    ellipse(x + i, y + 550, 10);
+    ellipse(x + i, y + 600, 10);
+    for (let i = 0; i < height; i = i + 25) {
+      fill(255, 255, 255, 5);
+      noStroke();
+      ellipse(x, y + i, 10);
+      ellipse(x + 50, y + i, 10);
+      ellipse(x + 100, y + i, 10);
+      ellipse(x + 150, y + i, 10);
+      ellipse(x + 200, y + i, 10);
+      ellipse(x + 250, y + i, 10);
+      ellipse(x + 300, y + i, 10);
+      ellipse(x + 350, y + i, 10);
+      ellipse(x + 400, y + i, 10);
+      ellipse(x + 450, y + i, 10);
+      ellipse(x + 500, y + i, 10);
+      ellipse(x + 550, y + i, 10);
+      ellipse(x + 600, y + i, 10);
+      ellipse(x + 650, y + i, 10);
+    }
+  }
+  stroke(255, 255, 255);
+  strokeWeight(4);
+  fill(50, 205, 50);
+  textSize(150);
+  textStyle(BOLD);
+  text("PLAYER2", width / 2 - 335, 300);
+  text("WINS", width / 2 - 200, 450);
+}
+
+function mouseClicked() {
+  if (
+    mouseClicked &&
+    mouseX > 230 &&
+    mouseX < 430 &&
+    mouseY > 300 &&
+    mouseY < 330
+  ) {
+    console.log("clicked");
+    screen++;
+  }
+}
 
 function reset() {
   if (puck.x >= width - 12.5) {
@@ -110,17 +280,39 @@ function draw() {
   playball();
   ballbounce();
   reset();
+  if (screen === 0) {
+    Startscreen();
+    mouseClicked();
+  }
+  if (screen === 3) {
+    WinscreenP1();
+    rotation = rotation - 0.01;
+    if (rotation <= 0) {
+      rotation = 0;
+      screen = 0;
+    }
+  }
+  if (screen === 4) {
+    WinscreenP2();
+    rotation = rotation - 0.01;
+    if (rotation <= 0) {
+      rotation = 0;
+      screen = 0;
+    }
+  }
 
   //Score-Reset
   if (Score[0] >= 10) {
     Score[0] = 0;
     Score[1] = 0;
     r = 200;
+    screen = 3;
   }
   if (Score[1] >= 10) {
     Score[0] = 0;
     Score[1] = 0;
     r = 200;
+    screen = 4;
   }
   //Arrow up right side
   if (keyIsDown(38)) {
